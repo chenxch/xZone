d3bcc61ee065f1f68509a163102ca3c0d1bcc6d0
xiaochenchen <xiaochen.chen@igg.com>
2022-01-24 00:01:56 +0800

    init

---
+++ background.js
@@ -0,0 +1,5 @@
+/*
+ * @Description: 
+ * @Author: chenxch
+ * @Date: 2022-01-22 23:47:24
+ */
---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+/*
+ * @Description: 
+ * @Author: chenxch
+ * @Date: 2022-01-23 23:16:35
+ */
+PetiteVue.createApp().mount()
